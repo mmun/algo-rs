307410f6475afa13722213987d728e4752be3ca5
Martin Muñoz <im.mmun@gmail.com>
2016-11-21 02:33:47 -0500

    finish bst

--- src/binary_search_tree.rs
+++ src/binary_search_tree.rs
@@ -1,134 +1,113 @@
 use std::fmt::Debug;
 use std::cmp::Ordering::*;
-use std::mem::{self, transmute};
 
-enum FindStrategy {
-    Any,
-    Before,
-    After
-}
+enum Find { Any, After }
 
 #[derive(Debug)]
-pub struct Node<K> {
+struct Node<K> {
     key: K,
     left: Tree<K>,
     right: Tree<K>,
 }
 
 #[derive(Debug)]
-pub enum Tree<K> {
-    Node(Box<Node<K>>),
-    Empty
+pub struct Tree<K> {
+    inner: Option<Box<Node<K>>>
 }
 
 impl<K: Ord+Debug> Tree<K> {
-    fn leaf(key: K) -> Tree<K> {
-        Tree::Node(Box::new(Node {
+   pub fn new() -> Tree<K> {
+        Tree { inner: None }
+    }
+
+    pub fn leaf(key: K) -> Tree<K> {
+        let node = Node {
             key: key,
-            left: Tree::Empty,
-            right: Tree::Empty
-        }))
+            left: Tree::new(),
+            right: Tree::new()
+        };
+
+        Tree { inner: Some(Box::new(node)) }
     }
 
-    fn take(&mut self) -> Self {
-        mem::replace(self, Tree::Empty)
+    pub fn has(&mut self, key: K) -> bool {
+        let tree = self.find_mut(&key, Find::Any);
+
+        tree.is_node()
     }
 
-    fn find_mut(&mut self, key: &K, strategy: FindStrategy) -> &mut Self {
+    pub fn insert(&mut self, key: K) {
+        let tree = self.find_mut(&key, Find::After);
+
+        *tree = Tree::leaf(key);
+    }
+
+    pub fn delete(&mut self, key: K) {
+        let tree = self.find_mut(&key, Find::Any); 
+
+        if tree.is_node() {
+            if tree.right_mut().is_node() {
+                let succ = {
+                    let succ = tree.right_mut().first_mut();
+                    let mut taken = succ.take();
+                    *succ = taken.right_mut().take();
+                    taken
+                };
+                tree.node_mut().key = succ.into_key();
+            } else {
+                *tree = tree.left_mut().take();
+            }
+        }
+    }
+
+    fn find_mut(&mut self, key: &K, strategy: Find) -> &mut Self {
         let mut cur = self;
 
         loop {
             let temp = cur;
-            if temp.inner.is_some() {
-                cur = match temp.inner.as_mut().unwrap().key.cmp(key) {
-                    Less    => &mut temp.inner.as_mut().unwrap().right,
-                    Greater => &mut temp.inner.as_mut().unwrap().left,
+            if temp.is_node() {
+                cur = match temp.key().cmp(key) {
+                    Less    => temp.right_mut(),
+                    Greater => temp.left_mut(),
                     Equal   => {
                         match strategy {
-                            FindStrategy::Any    => temp,
-                            FindStrategy::Before => &mut temp.inner.as_mut().unwrap().left,
-                            FindStrategy::After  => &mut temp.inner.as_mut().unwrap().right
+                            Find::After => temp.right_mut(),
+                            Find::Any   => return temp
                         }
                     }
                 };
             } else {
-                return temp;
+                return temp
             }
         }
-        
-        cur
     }
 
     fn first_mut(&mut self) -> &mut Self {
-        let mut cur = self;
+        if self.is_empty() { return self }
 
+        let mut cur = self;
         loop {
             let temp = cur;
-            cur = match temp.inner {
-                Some(ref mut node) => &mut node.left,
-                None               => return temp,
-            }
-        }
-
-        cur
-    }
-
-    pub fn has(&mut self, key: K) -> bool {
-        match *self.find_mut(&key, FindStrategy::Any) {
-            Tree::Node(_) => true,
-            Tree::Empty => false
-        }
-    }
-
-    pub fn insert(&mut self, key: K) {
-        let tree = self.find_mut(&key, FindStrategy::After);
-
-        match *tree {
-            Tree::Node(_) => unreachable!(),
-            Tree::Empty => *tree = Tree::leaf(key)
-        }
-    }
-
-    pub fn delete(&mut self, key: K) {
-        let tree = self.find_mut(&key, FindStrategy::Any);
-        
-        *tree = if let Tree::Node(mut node) = tree.take() {
-            if let Tree::Node(_) = node.right {
-                let left = node.left.take();//first_mut();
-                
-                let mut right = node.right.take();//first_mut();
-
-                let succ = {
-                    let mut succ_slot = right.first_mut();
-                    let mut succ = succ_slot.take();
-
-                    *succ_slot = if let Tree::Node(ref mut node) = succ {
-                        node.right.take()
-                    } else {
-                        unreachable!();
-                    };
-
-                    succ
-                };
-                
-                if let Tree::Node(succ_node) = succ {
-                    node.key = succ_node.key;
-                    node.left = left;
-                    node.right = right;
-                } else {
-                    unreachable!();
-                }
-                // node.left = left;
-                // node.right = right.right
-
-                Tree::Node(node)
+            if temp.left_mut().is_node() {
+                cur = temp.left_mut()
             } else {
-                node.left
+                return temp
             }
-        } else {
-            return; // The key wasn't in the tree
-        };
+        }
     }
+
+    fn into_node(self)      -> Node<K>      { *self.inner.unwrap() }
+    fn into_key(self)       -> K            { self.into_node().key }
+
+    fn take(&mut self)      -> Self         { Tree { inner: self.inner.take() } }
+    fn node_mut(&mut self)  -> &mut Node<K> { self.inner.as_mut().unwrap() }
+    fn left_mut(&mut self)  -> &mut Self    { &mut self.node_mut().left }
+    fn right_mut(&mut self) -> &mut Self    { &mut self.node_mut().right }
+
+    fn node(&self)          -> &Node<K>     { self.inner.as_ref().unwrap() }
+    fn key(&self)           -> &K           { &self.node().key }
+    fn is_node(&self)       -> bool         { self.inner.is_some() }
+    fn is_empty(&self)      -> bool         { self.inner.is_none() }
 }
 
 #[cfg(test)]
@@ -137,7 +116,7 @@ mod tests {
 
     #[test]
     fn insert() {
-        let mut bst = Tree::Empty;
+        let mut bst = Tree::new();
 
         bst.insert(5);
         bst.insert(2);
@@ -159,7 +138,7 @@ mod tests {
 
     #[test]
     fn insert_and_delete() {
-        let mut bst = Tree::Empty;
+        let mut bst = Tree::new();
 
         const F: bool = false;
         const T: bool = true;
@@ -217,7 +196,7 @@ mod tests {
 
     #[test]
     fn insert_and_delete_duplicates() {
-        let mut bst = Tree::Empty;
+        let mut bst = Tree::new();
 
         bst.insert(0);
         bst.insert(1);
@@ -225,18 +204,16 @@ mod tests {
         bst.insert(1);
         bst.insert(0);
         bst.insert(1);
+
+        println!("{:#?}", bst);
 
         assert_eq!(
             (0..2).map(|i| bst.has(i)).collect::<Vec<_>>(),
             [true, true]
         );
 
-        // println!("{:#?}", bst);
-        // println!("====================================================");
-
         bst.delete(0);
-        // println!("{:#?}", bst);
-        // println!("########################################################");
+        println!("{:#?}", bst);
         bst.delete(0);
         bst.delete(1);
 
